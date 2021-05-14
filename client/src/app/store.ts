2cd80debe1aa893ba25750c5af084df1c9dc3c12
Eigently <eigentlydev@gmail.com>
2021-05-14 13:12:31 -0400

    feat: add persistence to localstorage

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,13 +1,22 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import { reducer as themeReducer } from "../features/theme/themeSlice";
 import { reducer as e6bReducer } from "../features/e6b/e6bSlice";
+import { loadState, saveState } from "./localStorage";
+import { throttle } from "lodash";
 
 export const store = configureStore({
   reducer: {
     theme: themeReducer,
     e6b: e6bReducer,
   },
+  preloadedState: loadState(),
 });
+
+const throttledSaveState = throttle(() => {
+  saveState(store.getState());
+}, 1000);
+
+store.subscribe(throttledSaveState);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
