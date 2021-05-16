15dc5361d2c17de7e00f1c05b5e186b47e11ed12
Eigently <eigentlydev@gmail.com>
2021-05-16 13:57:17 -0400

    feat: add /version endpoint for git sha

--- server/src/main.rs
+++ server/src/main.rs
@@ -8,6 +8,14 @@ async fn health() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
+#[get("/version")]
+async fn version() -> impl Responder {
+    match std::env::var("COMMIT_SHA") {
+        Ok(sha) => HttpResponse::Ok().body(sha),
+        Err(_) => HttpResponse::Ok().body("dev"),
+    }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init();
@@ -16,6 +24,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(Logger::default())
             .service(health)
+            .service(version)
             .configure(d_atis::routes::config)
     })
     .bind("0.0.0.0:8080")?
