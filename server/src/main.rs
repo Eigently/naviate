f48b633139ad2481d755b3c70fe8a893a8704e6f
Eigently <eigentlydev@gmail.com>
2021-05-16 19:40:06 -0400

    feat: allow any origin for running in dev

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,4 @@
+use actix_cors::Cors;
 use actix_web::{get, middleware::Logger, App, HttpResponse, HttpServer, Responder};
 extern crate env_logger;
 
@@ -16,6 +17,20 @@ async fn version() -> impl Responder {
     }
 }
 
+fn get_cors() -> Cors {
+    match std::env::var("RUST_ENV") {
+        Ok(x) => match x.as_str() {
+            "development" | "dev" => Cors::default()
+                .allow_any_origin()
+                .allow_any_header()
+                .allow_any_method(),
+            "production" => Cors::default(),
+            _ => Cors::default(),
+        },
+        _ => Cors::default(),
+    }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init();
@@ -23,6 +38,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .wrap(Logger::default())
+            .wrap(get_cors())
             .service(health)
             .service(version)
             .configure(d_atis::routes::config)
