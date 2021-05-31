b3e3ed3eecfd40601ab848c11df6218b3dc977ba
Eigently <eigentlydev@gmail.com>
2021-05-31 09:59:26 -0500

    fix: add head information

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { Provider } from "react-redux";
 import { ChakraProvider } from "@chakra-ui/react";
 
@@ -7,6 +8,37 @@ import { theme } from "../styles/theme";
 export const App = ({ Component, pageProps }) => {
   return (
     <Provider store={store}>
+      <Head>
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="Naviate EFB" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#3a0ca3" />
+        <meta name="msapplication-config" content="/browserconfig.xml" />
+        <meta name="theme-color" content="#3a0ca3" />
+        <title>Naviate</title>
+      </Head>
       <ChakraProvider theme={theme}>
         <Component {...pageProps} />
       </ChakraProvider>
